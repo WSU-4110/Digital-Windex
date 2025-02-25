73947792abc259683dfcb44f5eafa97a2617e04f
CVbug <claraverani14@gmail.com>
2025-02-25 01:12:23 -0500

    Update DigiWindex.Factory.cs
    Reverted back from commit that differentiated constructors for tasks, theyre both just createTask()

--- DigiWindex.Factory.cs
+++ DigiWindex.Factory.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
@@ -10,7 +10,7 @@ public interface Task_ProductInterface
 {
     String startdir;
     //function to run command applies to all tasks
-    public void RunCommandDirectly(string startdir, string arguments)
+    private void RunCommandDirectly(string startdir, string arguments)
     {
            ProcessStartInfo processInfo = new ProcessStartInfo
            {
@@ -35,17 +35,17 @@ public interface Task_ProductInterface
 }
 
 //Concrete Creators
-abstract class abstract class TaskNoWarnings_Concrete_Creator
+abstract class TaskNoWarningsConcrete_Creator
 {
-    public abstract Task_ProductInterface createTaskNoWarnings(string startdir, string arguments)
+    public abstract Task_ProductInterface createTask(string startdir, string arguments)
     {
         startdir = Path.Combine(Environment.SystemDirectory, startdir);
     }
 }
 
-abstract class TaskYesWarnings_Concrete_Creator
+abstract class TaskYesWarningsConcrete_Creator
 {
-    public abstract Task_ProductInterface createTaskYesWarnings(string startdir, string arguments)
+    public abstract Task_ProductInterface createTask(string startdir, string arguments)
     {
         startdir = Path.Combine(Environment.SystemDirectory, startdir);
     }

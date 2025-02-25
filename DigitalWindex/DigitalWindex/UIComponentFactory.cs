b7bba1291a9256bf980f6f97a4478cd96bb3f4ab
Kaavya Karnati <kaavya.karnati@yottaasys.com>
2025-02-25 17:07:54 -0500

    UI Factory Method Design Patten

---
+++ DigitalWindex/DigitalWindex/UIComponentFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace DesktopApp
+{
+    public static class UIComponentFactory
+    {
+        public static Control GetComponent(string componentType)
+        {
+            switch (componentType.ToLower())
+            {
+                case "diagnostics":
+                    return new DiagnosticsControl();  // Uses the existing class
+                case "clean corruption":
+                    return new CleanCorruptionForm();  // Uses the existing class
+                default:
+                    throw new ArgumentException($"Unknown component type: {componentType}");
+            }
+        }
+    }
+}

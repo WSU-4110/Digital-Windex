36a718f963975602de17604caff37748b4fc7540
Reshma Shaik <127569816+reshma010@users.noreply.github.com>
2025-03-31 16:43:41 -0400

    Initial commit or update from local project

---
+++ DigitalWindex/DigitalWindex/WindowsRecoveryLauncher.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+
+namespace DigitalWindex.Backend
+{
+    public static class WindowsRecoveryLauncher
+    {
+        /// <summary>
+        /// Opens the Windows 10 and 11 "System -> Recovery" Settings page.
+        /// </summary>
+        public static void OpenRecoverySettings()
+        {
+            ProcessStartInfo psi = new ProcessStartInfo("ms-settings:recovery")
+            {
+                UseShellExecute = true, 
+                Verb = "open"
+            };
+            Process.Start(psi);
+        }
+    }
+}

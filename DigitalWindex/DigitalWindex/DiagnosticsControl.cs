cb37a8029ca0e4a5632b8668e04f1b07224dc2c6
Harini Kandi <hk6231@wayne.edu>
2025-03-31 17:36:37 -0400

    New change made in Diagnostics Control file. Also added System Diagnostics file to run the program

--- DigitalWindex/DigitalWindex/DiagnosticsControl.cs
+++ DigitalWindex/DigitalWindex/DiagnosticsControl.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿/*
+using System;
 using System.Windows.Forms;
 
 namespace DesktopApp
@@ -19,6 +20,47 @@ namespace DesktopApp
         {
             // Action when "Run Diagnostics" button is clicked
             MessageBox.Show("Diagnostics process started...", "System Diagnostics", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
+*/
+
+using System;
+using System.Windows.Forms;
+using SystemDiagnostics;
+
+namespace DesktopApp
+{
+    public partial class DiagnosticsControl : UserControl
+    {
+        public DiagnosticsControl()
+        {
+            InitializeComponent();
+        }
+
+        private void DiagnosticsControl_Load(object sender, EventArgs e)
+        {
+            // Optional: Initialization logic upon loading the control
+        }
+
+        private void BtnRunDiagnostics_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string result = DiagnosticRunner.RunAllDiagnostics();
+
+                MessageBox.Show(result,
+                    "System Diagnostics Output",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error running diagnostics: " + ex.Message,
+                    "Execution Error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
         }
     }
 }
